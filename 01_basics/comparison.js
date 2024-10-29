6b08d37aa22c3bb83556e6b3f09f0ac39cd3820e
Aryan-png-coder <aryanthakur333o@gmail.com>
2024-10-30 02:17:20 +0530

    Create comparison.js

---
+++ 01_basics/comparison.js
@@ -0,0 +1,24 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" < 1);
+console.log("02" > 1);
+// automatically converts datatype
+
+console.log("2" === 2);// === is strict check and do not converts datatype
+
+
+// do not use these comperison bcz unpredictable output
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);// falls is case of undefined
