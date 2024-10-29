906eb2a86e38f357533a4d8a8aec072f5759f644
Aryan-png-coder <aryanthakur333o@gmail.com>
2024-10-30 01:29:26 +0530

    conversion and operations

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,23 @@
+let name = "aryan"
+
+console.log(typeof name);
+
+let valueInNumber = Number(name)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+let valueInBoolean = Boolean(valueInNumber);
+console.log(typeof valueInBoolean);
+console.log(valueInBoolean);
+
+
+let someNumber = 43
+ 
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+// NaN => not a number
+// 1 => true , 0 => false
+//"43" => 43
---
+++ 01_basics/04_operation.js
@@ -0,0 +1,37 @@
+let value = 5
+let negvalue = -value
+console.log(negvalue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%10);
+console.log(2**4);// for to the power
+
+
+let str1 = "ARYAN"
+let str2 = " THAKUR"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + "2" + 3);
+console.log(1 + (2 + "3"));
+console.log(1 + 2 + "3");
+console.log(+true);
+console.log(+"");
+
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 5
+console.log(num1*num2/num3);
+
+
+let gameCounter = 0
+++gameCounter;
+console.log(gameCounter);
