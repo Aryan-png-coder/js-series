6f6bd0c67aebdd7046fbda7853cb8067ed331a2d
Aryan-png-coder <aryanthakur333o@gmail.com>
2024-10-30 00:01:16 +0530

    Create 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) // we are using nodeJS, not browser not works
+
+//console.log(3+3); // code readability shoud be high
+let aryan = "HEllo, Aryan";
+console.log(aryan);
+console.log(3+7);
+console.log("nice to meet you aryan");
+
+
+
+// datatypes
+
+let name = "aryan"  //string // ""
+let age = 17  // number // value from 2 to power 53
+let isLoggedIn = true // boolean // true or false
+let state = null // null // standalone value // output is object
+let city; // undefined // output is undefined
+// symbol => unique
+//bigint => if number is very big
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof age);
+
+
+
